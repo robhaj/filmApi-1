5a598b4096cccc3df9fb377d248ea7cf98897060
Robert Hajek <hajek.rob@gmail.com>
2015-10-05 19:03:08 -0600

    initial commit

---
+++ client/app.js
@@ -0,0 +1,24 @@
+var app = angular.module("app", ["ngRoute"]);
+
+app.config(function($routeProvider) {
+  $routeProvider
+  .when("/", {
+    templateUrl: "./views/search.html",
+    controller: "SearchController"
+  })
+  .when("/recommend", {
+    templateUrl: "./views/recommend.html",
+    controller: "RecommendController"
+  })
+  .when("/library", {
+    templateUrl: "./views/library.html",
+    controller: "LibraryController"
+  })
+  .when("/watchlist", {
+    templateUrl: "./views/watchlist.html",
+    controller: "WatchlistController"
+  })
+  .otherwise({
+    redirectTo: "/"
+  });
+});
---
+++ server/routes/api.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var router = express.Router();
+var mongoose = require('mongoose');
+
+// get ALL users
+router.get('/users', function(req, res, next) {
+});
+
+
+
+module.exports = router;
